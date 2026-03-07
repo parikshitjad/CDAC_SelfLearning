b7938a153256fc0cb6f0edcdd4e2f78795caafca
parikshitjad <159987621+parikshitjad@users.noreply.github.com>
2026-03-07 14:08:44 +0530

    Add files via upload

---
+++ C++/Operators.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10, b=5;
+
+    cout<<"Values: a = "<< a << " and b = "<< b << endl;
+
+    // Arithmetic Operators
+    cout<<"\nArithmetic Operators:"<<endl;
+    cout<<"Addition = "<< a+b << endl;
+    cout<<"Substraction = "<< a-b << endl;
+    cout<<"Multiplication = "<< a*b << endl;
+    cout<<"Division = "<< a/b << endl;
+    cout<<"Modulus = "<< a%b << endl;
+
+    // Unary Operators
+    cout<<"\nUnary Operators:"<<endl;
+    cout<<"PreIncrement = "<< ++a << endl;
+    cout<<"PostIncrement = "<< a++ << endl;
+    cout<<"PreDecrement = "<< --a << endl;
+    cout<<"PostDecrement = "<< a-- << endl;
+
+    // ternary Operators
+    cout<<"\nTernary Operators:"<<endl;
+    cout<<"Ternary = "<< (a>b ? "a is greater than b" : "b is greater than a") << endl;
+
+    // Assignment Operators
+    cout<<"\nAssignment Operators:"<<endl;
+    int c;
+    
+}
